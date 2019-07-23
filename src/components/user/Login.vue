bd9a5bfc9c1f843ef97372c913eacec16346f554
Nick Chan <nick.chan@primeplusglobal.com>
2019-07-23 18:48:33 +0800

    doLogin

--- src/components/user/Login.vue
+++ src/components/user/Login.vue
@@ -2,46 +2,48 @@
   <div class="inputLogin">
     <div>
       <span>id:</span>
-      <input type="text" name="id" id="username" v-bind="username" />
+      <input type="text" name="username" id="username" v-model="username" />
     </div>
     <div>
       <span>password:</span>
-      <input type="text" name="password" id="password" v-bind="password" />
+      <input type="text" name="password" id="password" v-model="password" />
     </div>
     <div>
       <input type="button" value="login" @click="doLogin" />
     </div>
-    <div>
-        {{ resData }}
-    </div>
+    <div>{{ resData }}</div>
   </div>
 </template>
 
 <script>
-const axios = require('axios');
+const axios = require("axios");
 
 export default {
   data() {
     return {
-      username: '',
-      password: '',
-      resData: ''
+      username: "",
+      password: "",
+      resData: ""
     };
   },
   methods: {
     doLogin() {
-      axios
-        .post("http://169.63.222.107:1337/login", {
+      axios({
+        method: "post",
+        url: "http://169.63.222.107:1337/login",
+        responseType: "json",
+        data: {
           username: this.username,
           password: this.password
-        })
+        }
+      })
         .then(res => {
-            this.resData = res;
+          this.resData = res.data;
         })
         .catch(err => {
-            this.resData = err;
+          this.resData = "ERROR: " + err.data;
         });
-    }
+    },
   }
 };
 </script>
